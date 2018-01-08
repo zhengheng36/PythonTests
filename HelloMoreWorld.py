fa4ecd86608911ab167fbdd9c6e62db764181454
Aaron.Zheng <zhengheng36@gmail.com>
2018-01-08 12:17:39 -0500

    Add HelloMoreWorld.py

---
+++ HelloMoreWorld.py
@@ -0,0 +1,5 @@
+print('Try More', 'Things', 'Now')
+
+inputString = input('The Terminal is waiting for your input...')
+
+print('Your input is:', inputString) #??? why the format put cannot display???
