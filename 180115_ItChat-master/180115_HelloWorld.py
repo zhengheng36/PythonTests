582147a255472a531de27341c8aebf6fd5329be4
Aaron.Zheng <zhengheng36@gmail.com>
2018-01-16 10:39:04 -0500

    Add

--- 180115_ItChat-master/180115_HelloWorld.py
+++ 180115_ItChat-master/180115_HelloWorld.py
@@ -1,5 +1,9 @@
 import itchat
 
-itchat.auto_login()
+##itchat.auto_login()
 
-itchat.send('This message send by Aaron.Zheng', toUserName='filehelper')
+##itchat.send('This message send by Aaron.Zheng', toUserName='filehelper')
+
+print()
+itchat.auto_login(enableCmdQR=True)
+print()
--- ProjectEuler/180115_9.py
+++ ProjectEuler/180115_9.py
@@ -1,5 +1,17 @@
 
+# Special Pythagorean triplet
+# Problem 9 
+# A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
 
+# a2 + b2 = c2
+# For example, 32 + 42 = 9 + 16 = 25 = 52.
+
+# There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+# Find the product abc.
+
+
+
+# It: 利用数学方法，首先做了一个因式分解，简化问题
 b = 1
 for a in range(200, 1000000):
     b = 1
@@ -14,6 +26,11 @@ for a in range(200, 1000000):
         break
 
         
-print(a)
-print(b)
-print(a*a + b*b)
+print('a:', a)
+print('b:', b)
+
+# KEY: in order to use 'square'
+from math import sqrt
+c = sqrt(a * a + b * b)
+print('c:', int(c))
+
