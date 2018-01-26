20ce9c773369c467411c3f541a4dd29beab5168c
Aaron.Zheng <zhengheng36@gmail.com>
2018-01-26 11:06:43 -0500

    AA

--- 180123_QRCode/Decoder/180123_TestOnly2.py
+++ 180123_QRCode/Decoder/180123_TestOnly2.py
@@ -1,22 +1,21 @@
 # Ref: http://pyqrcode.sourceforge.net/
 
 
-
 import sys, qrcode, pyqrcode
 
 picPath = 'C:\\Users\\Aaron.Zheng\\Desktop\\Reading PDF\\BackUpSpace\\Self-Learning\\ProgramCode\\Python\\InstallsPackage\\QRPic.jpg'
 
-# d = qrcode.Decoder()
+d = qrcode.Decoder()
 
-# if d.decode(picPath):
-#     print('result\: ' + d.result)
-# else:
-#     print('error: ' + d.error)
+if d.decode(picPath):
+    print('result\: ' + d.result)
+else:
+    print('error: ' + d.error)
 
-cc = pyqrcode.QRCode(picPath)
+# cc = pyqrcode.QRCode(picPath)
 
-print(cc)
-print(cc.data)
+# print(cc)
+# print(cc.data)
 
 
 
